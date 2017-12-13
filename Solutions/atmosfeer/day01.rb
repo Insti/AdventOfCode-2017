8a0f71c04756c39ddbf75af5deb8e153ff83b4e1
Nicolas Feer <atmosfeer@gmail.com>
2017-12-13 19:14:58 +0100

    Fixed some naming errors

--- Solutions/atmosfeer/day01.rb
+++ Solutions/atmosfeer/day01.rb
@@ -1,10 +1,4 @@
-digits = ""
-
-File.open("day02.input").each do |line|
-  digits += line.strip
-end
-
-digits = digits.chars.map(&:to_i)
+digits = File.open("day01.input").read.char.map(&:to_i)
 
 part1 = 0
 part2 = 0
@@ -14,7 +8,6 @@ digits.each_with_index do |n, i|
   part1 += n if n == digits[(i + 1) % digits.size]
 end
 
-# counter += digits.last if digits.last == digits.first
 
 puts "Part 1 Solution: #{part1}"
 puts "Part 2 Solution: #{part2}"
