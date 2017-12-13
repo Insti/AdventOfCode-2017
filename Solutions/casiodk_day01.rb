94f8b402735e6e82315660ed02503fe8b3625031
Nicolai Seerup <mail@soundtracktor.com>
2017-12-13 18:59:22 +0100

    refactor

--- Solutions/casiodk_day01.rb
+++ Solutions/casiodk_day01.rb
@@ -12,21 +12,24 @@ class CodeExample
 
   private
 
-  attr_reader :sum, :str
+  attr_reader :sum, :str, :index
 
   def last_index
     @last_index ||= str.size - 1
   end
 
   def calculate_sum
-    str.each_char.with_index do |char, index|
-      if str[index].to_i == str[next_char(index)].to_i
-        @sum += str[index].to_i
-      end
+    str.each_char.with_index do |char, i|
+      @index = i
+      @sum += str[index].to_i if is_a_match?
     end
   end
 
-  def next_char(index)
+  def next_char
     last_index == index ? 0 : index + 1
+  end
+
+  def is_a_match?
+    str[index].to_i == str[next_char].to_i
   end
 end
